ed7701934c2ec129bbb0e75c80d3498df301012e
Bartlomiej Lanczyk <bartlomiejl@backbase.com>
2025-11-11 08:13:42 +0100

    Initial project

---
+++ BoM/BoM/BoMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoMApp.swift
+//  BoM
+//
+//  Created by Bartlomiej Lanczyk on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BoMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoM/BoMTests/BoMTests.swift
@@ -0,0 +1,16 @@
+//
+//  BoMTests.swift
+//  BoMTests
+//
+//  Created by Bartlomiej Lanczyk on 11/11/2025.
+//
+
+import Testing
+
+struct BoMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
